131cfd043261fb7aa44f7067ab406b2dcae3165c
shamilbi <shamilbi@gmail.com>
2025-11-22 20:27:27 +0200

    +RequestCache

--- morgan/utils.py
+++ morgan/utils.py
@@ -1,4 +1,7 @@
+import json
 import re
+import urllib.parse
+import urllib.request
 
 from packaging.requirements import Requirement
 
@@ -42,3 +45,31 @@ class Cache:  # pylint: disable=protected-access
             if all(spec.operator in ('>', '>=') for spec in specifier._specs):
                 return True
         return False
+
+
+class RequestCache:  # pylint: disable=too-few-public-methods
+    d: dict[str, dict] = {}  # name: data
+
+    def get(self, url: str, name: str) -> dict:
+        if name in self.d:
+            return self.d[name]
+
+        if not url.endswith('/'):
+            url += '/'
+
+        # get information about this package from the Simple API in JSON
+        # format as per PEP 691
+        request = urllib.request.Request(
+            f"{url}{name}/",
+            headers={
+                "Accept": "application/vnd.pypi.simple.v1+json",
+            },
+        )
+
+        with urllib.request.urlopen(request) as response:
+            data = self.d[name] = json.load(response)
+            data['response_url'] = str(response.url)
+            return data
+
+
+RCACHE = RequestCache()
