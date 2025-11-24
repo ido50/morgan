e60a98d04b85f9d186e6ee587231e0acdaf72fce
shamilbi <shamilbi@gmail.com>
2025-11-24 17:45:15 +0200

    delete Cache

--- morgan/utils.py
+++ morgan/utils.py
@@ -4,8 +4,6 @@ import urllib.parse
 import urllib.request
 from typing import Dict
 
-from packaging.requirements import Requirement
-
 
 def to_single_dash(filename):
     'https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers'
@@ -21,31 +19,6 @@ def to_single_dash(filename):
         filename = filename[:m.start() + 1] + s2
     return filename
     # selenium-2.0.dev9429.tar.gz
-
-
-class Cache:  # pylint: disable=protected-access
-    def __init__(self):
-        self.cache: set[str] = set()
-
-    def check(self, req: Requirement) -> bool:
-        if self.is_simple_case(req):
-            return req.name in self.cache
-        return str(req) in self.cache
-
-    def add(self, req: Requirement):
-        if self.is_simple_case(req):
-            self.cache.add(req.name)
-        else:
-            self.cache.add(str(req))
-
-    def is_simple_case(self, req):
-        if not req.marker and not req.extras:
-            specifier = req.specifier
-            if not specifier:
-                return True
-            if all(spec.operator in ('>', '>=') for spec in specifier._specs):
-                return True
-        return False
 
 
 class RequestCache:  # pylint: disable=too-few-public-methods
